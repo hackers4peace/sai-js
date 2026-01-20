be9bf9fc2213331ae40f1c93ac8fa57e3d527d34
elf-pavlik <elf-pavlik@hackers4peace.net>
2026-01-20 17:43:01 -0600

    delete id package moved to services

--- packages/id/http.ts
+++
@@ -1,3 +0,0 @@
-import { serve } from '@hono/node-server'
-import { app } from './server.ts'
-serve(app)
--- packages/id/server.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test } from 'vitest'
-
-describe('subdomain with 303 redirect', () => {
-  test.todo('request root without subdomain')
-  test.todo('request root with subdomain')
-  test.todo('request path without subdomain')
-  test.todo('request path with subdomain')
-})
