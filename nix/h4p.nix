3737a9fb1fc14a832b4b2eac067296e5f2da90fa
elf-pavlik <elf-pavlik@hackers4peace.net>
2026-01-27 20:31:02 -0600

    [nix] enable firewal on h4p

--- nix/h4p.nix
+++ nix/h4p.nix
@@ -11,4 +11,17 @@
   };
   systemd.services.caddy.serviceConfig.EnvironmentFile =
     [ config.age.secrets.gandi.path ];
+
+  networking.firewall = {
+    enable = true;
+
+    interfaces.enp1s0.allowedTCPPorts = [ 22 80 443 ]; # SSH HTTP HTTPS
+    logRefusedConnections = false; # avoid log spam
+    checkReversePath = "loose";    # avoids Hetzner asymmetric routing issues
+
+    # Drop forwarded traffic from public NIC to podman
+    extraCommands = ''
+      iptables -A FORWARD -i enp1s0 -o podman0 -j DROP
+    '';
+  };
 }
